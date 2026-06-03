6a56dba56dfc4800497a441245f69e637e82e8e3
Federico Combi <fedecombi2000@gmail.com>
2026-06-03 12:15:16 +0200

    aggiunto scheletro progetto

--- esplorazione_dataset_gruppo/main.py
+++ esplorazione_dataset_gruppo/main.py
@@ -3,4 +3,6 @@
 # libreria scikit-lern
 # praticamente la parte EDA
 # normalizzare i dati
+# metti anche il grafico
+# fai in modo che sia impostabile con flask per mettere di valore e la sua risposta
 
